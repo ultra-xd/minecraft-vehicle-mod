750ece8dd89ad388fa0499253e1977ed1d009ccf
Aaron Tam <atam342007@gmail.com>
2025-06-03 23:12:13 -0400

    first commit, setup project

---
+++ src/main/java/net/ultra/vehiclemod/VehicleMod.java
@@ -0,0 +1,17 @@
+package net.ultra.vehiclemod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class VehicleMod implements ModInitializer {
+	public static final String MOD_ID = "vehiclemod";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/ultra/vehiclemod/VehicleModClient.java
@@ -0,0 +1,11 @@
+package net.ultra.vehiclemod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VehicleModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
